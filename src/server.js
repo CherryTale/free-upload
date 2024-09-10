a876788e3ec2d7fa391e5ed1f14ee2d75256ee06
zhuhaoran <qianran.zhr@alibaba-inc.com>
2024-09-10 14:13:05 +0800

    basic func

--- src/server.js
+++ src/server.js
@@ -1,28 +1,32 @@
 const os = require('os');
 const fs = require('fs').promises;
 const vscode = require('vscode');
+const http = require('http');
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const socketIo = require('socket.io');
 const QRCode = require('qrcode');
 const path = require('path');
 const { getFileNameWithTag } = require('./utils.js');
 
-let qrCodeRef = null;
-
 function createThenStartServer(ip, port, output) {
   const uploadURL = '/upload';
-  let uploadDir = vscode.workspace.getConfiguration('free-upload').get('uploadFolder', path.join(os.homedir(), 'uploads'));
-  if (!uploadDir) {
-    uploadDir = path.join(os.homedir(), 'uploads');
-  }
+  const chatURL = '/chat';
+  const uploadDir = vscode.workspace.getConfiguration('free-upload').get('uploadFolder', '') || path.join(os.homedir(), 'uploads');
+  let qrCodeRef = null;
+  // 用于存储聊天消息的数组
+  const messageHistory = [];
 
   const app = express();
+  const server = http.createServer(app);
+  const io = socketIo(server);
+
   app.use(fileUpload());
   app.use(express.static(path.join(__dirname, 'views', 'static')));
   app.set('view engine', 'ejs');
   app.engine('ejs', require('ejs').__express);
 
-  app.get(uploadURL, (req, res) => {
+  app.get('/', (req, res) => {
     res.render(path.join(__dirname, 'views', 'index.ejs'), {
       uploadRoute: `http://${ip}:${port}${uploadURL}`,
       qrCode: qrCodeRef,
@@ -33,7 +37,7 @@ function createThenStartServer(ip, port, output) {
       sourceAddr = sourceAddr.substr(7)
     }
     output.appendLine(`${sourceAddr} connected`);
-    vscode.window.showInformationMessage(`${sourceAddr} connected`);
+    // vscode.window.showInformationMessage(`${sourceAddr} connected`);
   });
 
   app.post(uploadURL, async (req, res) => {
@@ -52,37 +56,59 @@ function createThenStartServer(ip, port, output) {
         const filePath = path.join(uploadDir, getFileNameWithTag(fileName));
         await fs.writeFile(filePath, file.data);
         output.appendLine('file://' + filePath);
-        vscode.window.showInformationMessage("Received " + fileName);
+        // vscode.window.showInformationMessage("Received " + fileName);
         return fileName;
       });
       const uploadedFileNames = await Promise.all(uploadPromises);
       res.send(`Files uploaded successfully: ${uploadedFileNames.join(', ')}`);
     } catch (err) {
-      output.appendLine('Error during file upload:' + err);
+      output.appendLine('Error during file upload: ' + err);
       res.status(500).send('An error occurred while uploading files.');
     }
   });
 
-  const server = app.listen(port, () => {
-    QRCode.toDataURL(`http://${ip}:${port}${uploadURL}`, { errorCorrectionLevel: 'H' }, (err, url) => {
+  io.on('connection', (socket) => {
+    const { address } = socket.handshake;
+    const userIp = address.substr(0, 7) === "::ffff:" ? address.substr(7) : address;
+    console.log('A user connected:', userIp);
+    // 将历史消息发送给新连接的客户端
+    socket.emit('chat history', messageHistory);
+
+    socket.on('chat message', (msg) => {
+      console.log(`Client: ${msg}`); // 在控制台显示来自客户端的消息
+
+      // 将新消息添加到历史数组
+      messageHistory.push(msg);
+
+      // 广播给所有连接的客户端
+      io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+      console.log(`User disconnected: ${userIp}`);
+    });
+  });
+
+  const runningServer = server.listen(port, () => {
+    QRCode.toDataURL(`http://${ip}:${port}`, { errorCorrectionLevel: 'H' }, (err, url) => {
       if (err) {
         output.appendLine(err);
         return;
       } else {
         qrCodeRef = url;
-        vscode.env.openExternal(`http://${ip}:${port}${uploadURL}`);
+        vscode.env.openExternal(`http://${ip}:${port}`);
       }
     });
   });
 
-  function stopServer() {
-    server.close(() => {
-      output.appendLine('Gracefully Shutdown');
-    });
+  const stopServer = () => {
+    io.emit('chat message', 'The server is shutting down.'); // 通知所有客户端服务器正在关闭
+    runningServer.close();
+    io.close();
+    output.appendLine('Server stopped');
   }
   process.on('SIGTERM', stopServer);
   process.on('SIGINT', stopServer);
-
   return stopServer;
 }
 
